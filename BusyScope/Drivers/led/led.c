f3ae1aedb14caae7d3c402560215b6d4de53ca2e
BusyBox <2894415997@qq.com>
2024-12-04 22:31:46 +0800

    [feat]完成LED驱动与LCD驱动移植

---
+++ BusyScope/Drivers/led/led.c
@@ -0,0 +1,38 @@
+//
+// Created by Mac on 2024/12/4.
+//
+
+#include "gpio.h"
+#include "stm32f4xx_hal.h"
+
+#include "led/led.h"
+
+void led_ds0_on()
+{
+    HAL_GPIO_WritePin(LED_DS0_GPIO_Port, LED_DS0_Pin, GPIO_PIN_RESET);
+}
+
+void led_ds0_off()
+{
+    HAL_GPIO_WritePin(LED_DS0_GPIO_Port, LED_DS0_Pin, GPIO_PIN_SET);
+}
+
+void led_ds0_toggle()
+{
+    HAL_GPIO_TogglePin(LED_DS0_GPIO_Port, LED_DS0_Pin);
+}
+
+void led_ds1_on()
+{
+    HAL_GPIO_WritePin(LED_DS1_GPIO_Port, LED_DS1_Pin, GPIO_PIN_RESET);
+}
+
+void led_ds1_off()
+{
+    HAL_GPIO_WritePin(LED_DS1_GPIO_Port, LED_DS1_Pin, GPIO_PIN_SET);
+}
+
+void led_ds1_toggle()
+{
+    HAL_GPIO_TogglePin(LED_DS1_GPIO_Port, LED_DS1_Pin);
+}
---
+++ BusyScope/Drivers/led/led.h
@@ -0,0 +1,18 @@
+//
+// Created by Mac on 2024/12/4.
+//
+
+#ifndef BUSYSCOPE_LED_H
+#define BUSYSCOPE_LED_H
+
+
+void led_ds0_on();
+void led_ds0_off();
+void led_ds0_toggle();
+
+void led_ds1_on();
+void led_ds1_off();
+void led_ds1_toggle();
+
+
+#endif //BUSYSCOPE_LED_H
